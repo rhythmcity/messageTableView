1949944e277f307301518e5cc317472f448b43ea
liyan <707704996@qq.com>
2014-04-29 18:08:05 +0800

    添加XMPP

--- mesageTableView/mesageTableView/AppDelegate.h
+++ mesageTableView/mesageTableView/AppDelegate.h
@@ -7,16 +7,44 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "XMPPFramework.h"
+@protocol ChatDelegate;
 #define iPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
+{
+    
+        XMPPStream *xmppStream;
+        XMPPRoster *xmppRoster;
+        XMPPRosterCoreDataStorage *xmppRosterStorage;
+        XMPPReconnect *xmppReconnect;
+        XMPPMessageArchivingCoreDataStorage *xmppMessageArchivingCoreDataStorage;
+        XMPPMessageArchiving *xmppMessageArchivingModule;
+    
+
+}
 
 
 @property (strong, nonatomic) UIWindow *window;
+@property (nonatomic,strong) id<ChatDelegate> chatDelegate;
+@property (nonatomic, strong) XMPPStream *xmppStream;
+@property (nonatomic, strong) XMPPRosterCoreDataStorage *xmppRosterStorage;
+@property (nonatomic, strong) XMPPRoster *xmppRoster;
+@property (nonatomic, strong) XMPPReconnect *xmppReconnect;
+@property (nonatomic, strong) XMPPMessageArchivingCoreDataStorage *xmppMessageArchivingCoreDataStorage;
+@property (nonatomic, strong) XMPPMessageArchiving *xmppMessageArchivingModule;
 @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
 - (void)saveContext;
 - (NSURL *)applicationDocumentsDirectory;
+
+
+
+@end
+@protocol ChatDelegate <NSObject>
+
+-(void)friendStatusChange:(AppDelegate *)appD Presence:(XMPPPresence *)presence;
+-(void)getNewMessage:(AppDelegate *)appD Message:(XMPPMessage *)message;
 
 @end
--- mesageTableView/mesageTableView/AppDelegate.m
+++ mesageTableView/mesageTableView/AppDelegate.m
@@ -149,5 +149,182 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+#pragma mark - xmpp
+- (void)setupStream{
+    xmppStream = [[XMPPStream alloc]init];
+    [xmppStream addDelegate:self delegateQueue:dispatch_get_main_queue()];
+    
+    xmppReconnect = [[XMPPReconnect alloc]init];
+    [xmppReconnect activate:self.xmppStream];
+    
+    xmppRosterStorage = [[XMPPRosterCoreDataStorage alloc]init];
+    xmppRoster = [[XMPPRoster alloc]initWithRosterStorage:xmppRosterStorage];
+    [xmppRoster activate:self.xmppStream];
+    [xmppRoster addDelegate:self delegateQueue:dispatch_get_main_queue()];
+    
+    xmppMessageArchivingCoreDataStorage = [XMPPMessageArchivingCoreDataStorage sharedInstance];
+    xmppMessageArchivingModule = [[XMPPMessageArchiving alloc]initWithMessageArchivingStorage:xmppMessageArchivingCoreDataStorage];
+    [xmppMessageArchivingModule setClientSideMessageArchivingOnly:YES];
+    [xmppMessageArchivingModule activate:xmppStream];
+    [xmppMessageArchivingModule addDelegate:self delegateQueue:dispatch_get_main_queue()];
+    
+}
+- (BOOL)myConnect{
+//    NSString *jid = [[NSUserDefaults standardUserDefaults]objectForKey:kMyJID];
+//    NSString *ps = [[NSUserDefaults standardUserDefaults]objectForKey:kPS];
+//    if (jid == nil || ps == nil) {
+//        return NO;
+//    }
+//    XMPPJID *myjid = [XMPPJID jidWithString:[[NSUserDefaults standardUserDefaults]objectForKey:kMyJID]];
+//    NSError *error ;
+//    [xmppStream setMyJID:myjid];
+//    if (![xmppStream connectWithTimeout:XMPPStreamTimeoutNone error:&error]) {
+//        NSLog(@"my connected error : %@",error.description);
+//        return NO;
+//    }
+    return YES;
+}
+
+#pragma mark - XMPPStreamDelegate
+
+- (void)xmppStreamWillConnect:(XMPPStream *)sender
+{
+    NSLog(@"xmppStreamWillConnect");
+}
+- (void)xmppStreamDidConnect:(XMPPStream *)sender
+{
+//    NSLog(@"xmppStreamDidConnect");
+//    if ([[NSUserDefaults standardUserDefaults]objectForKey:kPS]) {
+//        NSError *error ;
+//        if (![self.xmppStream authenticateWithPassword:[[NSUserDefaults standardUserDefaults]objectForKey:kPS] error:&error]) {
+//            NSLog(@"error authenticate : %@",error.description);
+//        }
+//    }
+}
+- (void)xmppStreamDidRegister:(XMPPStream *)sender
+{
+    NSLog(@"xmppStreamDidRegister");
+    
+//    if ([[NSUserDefaults standardUserDefaults]objectForKey:kPS]) {
+//        NSError *error ;
+//        if (![self.xmppStream authenticateWithPassword:[[NSUserDefaults standardUserDefaults]objectForKey:kPS] error:&error]) {
+//            NSLog(@"error authenticate : %@",error.description);
+//        }
+//    }
+}
+- (void)xmppStream:(XMPPStream *)sender didNotRegister:(NSXMLElement *)error
+{
+    //[self showAlertView:@"当前用户已经存在"];
+}
+- (void)xmppStreamDidAuthenticate:(XMPPStream *)sender
+{
+    NSLog(@"xmppStreamDidAuthenticate");
+    XMPPPresence *presence = [XMPPPresence presence];
+	[[self xmppStream] sendElement:presence];
+    
+}
+- (void)xmppStream:(XMPPStream *)sender didNotAuthenticate:(NSXMLElement *)error
+{
+    NSLog(@"didNotAuthenticate:%@",error.description);
+}
+- (NSString *)xmppStream:(XMPPStream *)sender alternativeResourceForConflictingResource:(NSString *)conflictingResource
+{
+    NSLog(@"alternativeResourceForConflictingResource: %@",conflictingResource);
+    return @"XMPPIOS";
+}
+- (BOOL)xmppStream:(XMPPStream *)sender didReceiveIQ:(XMPPIQ *)iq
+{
+    NSLog(@"didReceiveIQ: %@",iq.description);
+    
+    return YES;
+}
+- (void)xmppStream:(XMPPStream *)sender didReceiveMessage:(XMPPMessage *)message
+{
+    NSLog(@"didReceiveMessage: %@",message.description);
+    if ([self.chatDelegate respondsToSelector:@selector(getNewMessage:Message:)]) {
+        [self.chatDelegate getNewMessage:self Message:message];
+    }
+}
+- (void)xmppStream:(XMPPStream *)sender didReceivePresence:(XMPPPresence *)presence
+{
+    NSLog(@"didReceivePresence: %@",presence.description);
+    if (presence.status) {
+        if ([self.chatDelegate respondsToSelector:@selector(friendStatusChange:Presence:)]) {
+            [self.chatDelegate friendStatusChange:self Presence:presence];
+        }
+    }
+}
+- (void)xmppStream:(XMPPStream *)sender didReceiveError:(NSXMLElement *)error
+{
+    NSLog(@"didReceiveError: %@",error.description);
+}
+- (void)xmppStream:(XMPPStream *)sender didSendIQ:(XMPPIQ *)iq
+{
+    NSLog(@"didSendIQ:%@",iq.description);
+}
+- (void)xmppStream:(XMPPStream *)sender didSendMessage:(XMPPMessage *)message
+{
+    NSLog(@"didSendMessage:%@",message.description);
+}
+- (void)xmppStream:(XMPPStream *)sender didSendPresence:(XMPPPresence *)presence
+{
+    NSLog(@"didSendPresence:%@",presence.description);
+}
+- (void)xmppStream:(XMPPStream *)sender didFailToSendIQ:(XMPPIQ *)iq error:(NSError *)error
+{
+    NSLog(@"didFailToSendIQ:%@",error.description);
+}
+- (void)xmppStream:(XMPPStream *)sender didFailToSendMessage:(XMPPMessage *)message error:(NSError *)error
+{
+    NSLog(@"didFailToSendMessage:%@",error.description);
+}
+- (void)xmppStream:(XMPPStream *)sender didFailToSendPresence:(XMPPPresence *)presence error:(NSError *)error
+{
+    NSLog(@"didFailToSendPresence:%@",error.description);
+}
+- (void)xmppStreamWasToldToDisconnect:(XMPPStream *)sender
+{
+    NSLog(@"xmppStreamWasToldToDisconnect");
+}
+- (void)xmppStreamConnectDidTimeout:(XMPPStream *)sender
+{
+    NSLog(@"xmppStreamConnectDidTimeout");
+}
+- (void)xmppStreamDidDisconnect:(XMPPStream *)sender withError:(NSError *)error
+{
+    NSLog(@"xmppStreamDidDisconnect: %@",error.description);
+}
+#pragma mark - XMPPRosterDelegate
+- (void)xmppRoster:(XMPPRoster *)sender didReceivePresenceSubscriptionRequest:(XMPPPresence *)presence
+{
+//    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:presence.fromStr message:@"add" delegate:self cancelButtonTitle:@"cancle" otherButtonTitles:@"yes", nil];
+//    alertView.tag = tag_subcribe_alertView;
+//    [alertView show];
+}
+#pragma mark - XMPPReconnectDelegate
+- (void)xmppReconnect:(XMPPReconnect *)sender didDetectAccidentalDisconnect:(SCNetworkReachabilityFlags)connectionFlags
+{
+    NSLog(@"didDetectAccidentalDisconnect:%u",connectionFlags);
+}
+- (BOOL)xmppReconnect:(XMPPReconnect *)sender shouldAttemptAutoReconnect:(SCNetworkReachabilityFlags)reachabilityFlags
+{
+    NSLog(@"shouldAttemptAutoReconnect:%u",reachabilityFlags);
+    return YES;
+}
+//#pragma mark - my method
+//-(void)showAlertView:(NSString *)message{
+//    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:nil message:message delegate:self cancelButtonTitle:@"ok" otherButtonTitles:nil, nil];
+//    [alertView show];
+//}
+//#pragma mark - UIAlertView Delegate
+//- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+//{
+//    if (alertView.tag == tag_subcribe_alertView && buttonIndex == 1) {
+//        XMPPJID *jid = [XMPPJID jidWithString:alertView.title];
+//        [[self xmppRoster] acceptPresenceSubscriptionRequestFrom:jid andAddToRoster:YES];
+//        //        [self.xmppRoster rejectPresenceSubscriptionRequestFrom:<#(XMPPJID *)#>] ;
+//    }
+//}
+
 
 @end
